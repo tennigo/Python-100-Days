6248f51f2455dc6089fd686419c728837710acff
Tenni_Go <tennigo@gmail.com>
2020-01-05 21:23:58 +0800

    Day003_20200105

--- MyStudyNote/ex008_Piecewisefunction.py
+++ MyStudyNote/ex008_Piecewisefunction.py
@@ -4,12 +4,36 @@
 f(x) = x + 2 (-1 <= x <=1)
        5x + 3 (x < -1)
 """
+# ex1 用了elif
+# ex2 没用elif
 
-x = float(input('x = '))
-if x > 1:
-    y = 3 * x - 5
-elif x >= -1:
-    y = x + 2
+def ex1():
+    x = float(input('x = '))
+    if x > 1:
+        y = 3 * x - 5
+    elif x >= -1:
+        y = x + 2
+    else:
+        y = 5 * x + 3
+    print('f(%.2f) = %.2f' % (x, y))
+
+def ex2():
+    x = float(input('x = '))
+    if x > 1:
+        y = 3 * x - 5
+    else:
+        if x >= -1:
+            y = x + 2
+        else:
+            y = 5 * x + 3
+    print('f(%.2f) = %.2f' % (x, y))
+
+
+x = int(input('chice 1 or 2. : '))
+
+if x == 1:
+    ex1()
+elif x == 2:
+    ex2()
 else:
-    y = 5 * x + 3
-print('f(%.2f) = %.2f' % (x, y))
+    print('Error')
