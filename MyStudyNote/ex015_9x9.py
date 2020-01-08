bc609078252d1d241249297ca3a88637db57a01b
Tenni_Go <tennigo@gmail.com>
2020-01-08 22:07:12 +0800

    Day004_20200108

---
+++ MyStudyNote/ex015_9x9.py
@@ -0,0 +1,9 @@
+def main():
+    for i in range(1, 10):
+        for j in range(1, i + 1):
+            print('%d*%d=%d' % (i, j, i * j), end='\t')
+        print()
+
+if __name__ == '__main__':
+    main()
+
