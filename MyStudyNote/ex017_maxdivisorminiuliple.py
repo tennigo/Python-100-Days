ea4d58ada625838d5fb31ade954638434c9e49c8
Tenni_Go <tennigo@gmail.com>
2020-01-11 21:25:03 +0800

    Day004_20200111

---
+++ MyStudyNote/ex017_maxdivisorminiuliple.py
@@ -0,0 +1,15 @@
+x = int(input('x = '))
+y = int(input('y = '))
+# 如果x大于y就交换x和y的值
+if x > y:
+    # 通过下面的操作将y的值赋给x, 将x的值赋给y
+    x, y = y, x
+
+# print('x=%d, y=%d' % (x, y))
+
+# 从两个数中较小的数开始做递减的循环
+for factor in range(x, 0, -1):
+    if x % factor == 0 and y % factor == 0:
+        print('%d和%d的最大公约数是%d' % (x, y, factor))
+        print('%d和%d的最小公倍数是%d' % (x, y, x * y // factor))
+        break
